e2348d368ee8051c63201918084d5d80c1a42360
Junjie Cai <nickcaijunjie1991@gmail.com>
2023-09-27 10:24:51 +0200

    Initial Commit

---
+++ OpenHealthCardFrameworkTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OpenHealthCardFrameworkTest
+//
+//  Created by akami GmbH on 27.09.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OpenHealthCardFrameworkTest/OpenHealthCardFrameworkTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenHealthCardFrameworkTestApp.swift
+//  OpenHealthCardFrameworkTest
+//
+//  Created by akami GmbH on 27.09.23.
+//
+
+import SwiftUI
+
+@main
+struct OpenHealthCardFrameworkTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
